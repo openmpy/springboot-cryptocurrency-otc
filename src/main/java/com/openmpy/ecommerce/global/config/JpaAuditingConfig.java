651e4722f24ec4de9f1bd0428fad3adfdf25488a
openmpy <suhwan@kakao.com>
2024-06-11 22:41:10 +0900

    feat: Add BaseEntity class and JpaAuditingConfig

---
+++ src/main/java/com/openmpy/ecommerce/global/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.openmpy.ecommerce.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
---
+++ src/main/java/com/openmpy/ecommerce/global/entity/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.openmpy.ecommerce.global.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime updatedAt;
+}
